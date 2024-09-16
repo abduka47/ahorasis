e020f17b16997998f3cfd1327b2159d51f140fc6
Abdu Rieder <Abdu.Rieder@teamit.com.co>
2024-09-16 08:27:13 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ahorasis.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
